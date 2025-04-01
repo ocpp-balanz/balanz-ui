19c863d6711e163e0904942c712ba81cd02228bc
jens-markussen <jens.markussen@gmail.com>
2025-04-01 11:21:56 +0200

    First attempt. Better, but not quite right

--- src/services/balanz_api.ts
+++ src/services/balanz_api.ts
@@ -3,23 +3,49 @@ type CallFunction = {
     resolve: Function;
 };
 
+
+function sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export default class BalanzAPI {
     ws: WebSocket;
     outstanding_calls: Map<string, CallFunction>;
-    logged_in: boolean;
-    logged_in_user_type: string;
+    logged_in: boolean = false;
+    logged_in_user_type: string = "";
+    token: string;
+    url: string;
+    connected: boolean = false;
 
     constructor(url: string) {
-        this.ws = new WebSocket(url, ["ocpp1.6"]);
+        this.url = url;
+        this.token = "";
         this.outstanding_calls = new Map<string, CallFunction>();
+        this.ws = this._connect();
+    }
+
+    private async _wait_connected(): Promise<void> {
+        for (let i = 0; i < 30; i++) {
+            await sleep(100);
+            if (this.connected)
+                break;
+        }
+    }
+
+    private _connect(): Websocket {
         this.logged_in = false;
         this.logged_in_user_type = "";
+        this.connected = false;
+        this.ws = new WebSocket(this.url, ["ocpp1.6"]);
 
         this.ws.onclose = () => {
+            this.connected = false;
+            this.logged_in = false;
             console.log('balanz WebSocket closed');
         };
 
         this.ws.onopen = () => {
+            this.connected = true;
             console.log('balanz WebSocket connected');
         };
     
@@ -43,6 +69,7 @@ export default class BalanzAPI {
                 }
             }
         };
+        return this.ws;
     }
 
     static gen_message_id(): string {
@@ -55,7 +82,40 @@ export default class BalanzAPI {
         console.log("dummy called. LOGIC ERROR", data)
     }
 
+    async reconnect(): Promise<boolean> {
+        if (this.token == "")
+            return false; // no stored token. Would not be able to login.
+
+        // Try for some time before giving up.
+        for (let i = 0; i < 15; i++) {
+            // Then login again
+            this.ws.close();
+            this._connect();
+            await this._wait_connected();
+            if (!this.connected) {
+                await sleep(5000);  // Add delay to avoid reconnecting immediately
+                console.log("Reconnection failed. Trying again in 5 seconds.");
+            }
+            this.logged_in_user_type = await this.login(this.token);
+            if (this.logged_in_user_type == "") {
+                console.log("Failed to login again.");
+                return false;
+            }
+
+            console.log("Succesfully reconnected and logged in again");
+            return true;
+        }
+        return false;
+    }
+
     async call(command: string, payload: any): Promise<BalanzResult> {
+        if (!this.connected) {
+            const ok: boolean = await this.reconnect();
+            if (!ok) {
+                console.log("Failed to reconnect.");
+                return [0, null];
+            }
+        }
         const message_id = BalanzAPI.gen_message_id();
         try {
             const call_function: CallFunction = {resolve: BalanzAPI.dummy};
@@ -74,7 +134,14 @@ export default class BalanzAPI {
         }
     }
 
-    call_async(command: string, payload: unknown, callback: Function): void {
+    async call_async(command: string, payload: unknown, callback: Function): Promise<void> {
+        if (!this.connected) {
+            const ok: boolean = await this.reconnect();
+            if (!ok) {
+                console.log("Failed to reconnect.");
+                return;
+            }
+        }
         const message_id = BalanzAPI.gen_message_id();
         try {
             const call_function: CallFunction = {resolve: BalanzAPI.dummy};
@@ -95,6 +162,7 @@ export default class BalanzAPI {
             this.logged_in = true;
             this.logged_in_user_type = payload["user_type"];
             console.log("Succesfully logged in as " + this.logged_in_user_type);
+            this.token = token;
             return this.logged_in_user_type;
         } else {
             console.log("Login failed", payload);
